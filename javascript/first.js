bf47967c2ca80efa183f5e898b9d6c7fb48b95e4
Hieshs-sharma628 <hiteshsharma200@gmail.com>
2026-04-03 21:16:12 +0530

    first commit

---
+++ javascript/first.js
@@ -0,0 +1,17 @@
+console.log("hello")
+
+//variable declaration
+var a="hellow world!!"
+console.log(a)
+//array declaration
+var array=["Viren","Arman","Shiva","Rishab","Saran"]
+console.log(array)
+console.log("Lenght of array",array.lenght)
+array.push("hanuman")
+console.log("Array after push::",array)
+array.pop()
+console.log("Array after pop::",array)
+array.shift("Jasmit")
+console.log("Array after shift::",array)
+array.unshift("Jaskaran")
+console.log("Array after unshift::",array)
