b8b1da121aab3e255e3894353798832b0127356a
Hieshs-sharma628 <229311725+Hieshs-sharma628@users.noreply.github.com>
2026-04-04 21:38:16 +0530

    First commit

---
+++ javascript/object.js
@@ -0,0 +1,14 @@
+var student={
+Name: "Arman",
+semester: "IInd",
+course: "ВСА",
+rollno:101,
+}
+console.log(student)
+console.log(student.semester)
+delete student.rollno
+console.log(student)
+student.sesion=2026;
+console.log(student)
+student.course="AI"
+console.log(student)
