2146cf3f8dd77692054f5e1a8313f71147c7ed8a
Hieshs-sharma628 <229311725+Hieshs-sharma628@users.noreply.github.com>
2026-05-02 22:46:58 +0530

    first commit

--- javascript/object.js
+++ javascript/object.js
@@ -1,12 +1,12 @@
 var student={
-NaMe: "ArMan",
-seMester: "IInd",
+Name: "Arman",
+semester: "IInd",
 course: "ВСА",
-rollno:101,
+rollNo:101
 }
 console.log(student)
-console.log(student.seMester)
-delete student.rollno
+console.log(student.semester)
+delete student.rollNo
 console.log(student)
 student.sesion=2026;
 console.log(student)
